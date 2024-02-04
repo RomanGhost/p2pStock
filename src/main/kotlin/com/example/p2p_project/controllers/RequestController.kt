d2076535782aa50f6f3a71ee0b0de8b15a3b78de
Ghost <rildiarov@gmail.com>
2024-02-04 18:00:41 +0400

    Fix small errors

--- src/main/kotlin/com/example/p2p_project/controllers/RequestController.kt
+++ src/main/kotlin/com/example/p2p_project/controllers/RequestController.kt
@@ -4,15 +4,7 @@ import com.example.p2p_project.models.Request
 import com.example.p2p_project.services.RequestService
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.stereotype.Controller
-import org.springframework.web.bind.annotation.DeleteMapping
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.PutMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("\${application.info.api}/request")
