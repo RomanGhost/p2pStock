13edea949475bcd8228e92eb2b207a25f240f17c
Ghost <rildiarov@gmail.com>
2024-05-01 23:55:11 +0400

    add styles for pages

--- src/main/kotlin/com/example/p2p_project/models/Card.kt
+++ src/main/kotlin/com/example/p2p_project/models/Card.kt
@@ -9,7 +9,7 @@ data class Card(
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     var id: Long = 0,
 
-    @ManyToOne(fetch = FetchType.LAZY, cascade = [CascadeType.ALL])
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     var user: User = User(),
 
